c4657a860d9e8ee1803ff50b3044f6286c519d43
Hetal Sharma <hetalsharma9422@gmail.com>
2022-09-30 16:54:17 +0530

    create student scaffold

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'css/application.scss';
+import $ from 'jquery';
+global.$ = jQuery;
+import "bootstrap";
+
--- db/migrate/20220930094709_create_students.rb
+++ db/migrate/20220930094709_create_students.rb
@@ -2,9 +2,9 @@ class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
       t.string :first_name
-      t.string :string
       t.string :last_name
       t.string :email
+      t.datetime :date_of_birth
 
       t.timestamps
     end
